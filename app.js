c153ba3d653227006fffce7d1135270baa3cac27
mangochiman <mangochiman@gmail.com>
2015-10-30 09:57:06 +0200

    Working on sign_in page

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/sign_in', routes);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sign_in', function (req, res, next) {
+    res.render('sign_in');
+});
 module.exports = router;
